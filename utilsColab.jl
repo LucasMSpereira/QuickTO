5354f8f53792ae92213967cf912366594bca139e
Lucas Pereira <lucas.lmspereira@gmail.com>
2023-07-27 21:00:49 -0300

    remove makie and cairomakie

--- utilsColab.jl
+++ utilsColab.jl
@@ -7,7 +7,7 @@ if !packagesInstalled
     (
       "TopOpt", "Ferrite", "JLD2", "TimerOutputs", "StatsBase",
       "Parameters", "HDF5", "Statistics", "BSON", "ValueHistories",
-      "Poppler_jll", "MultivariateStats", "DataFrames", "Makie", "CairoMakie",
+      "Poppler_jll", "MultivariateStats", "DataFrames",
       "MLUtils", "Flux", "ExplainableAI", "ShapML", "Suppressor",
       "Zygote", "Optimisers", "ChainRulesCore", "Interpolations"
     )
@@ -19,7 +19,7 @@ using LinearAlgebra, Dates, TopOpt, Ferrite, JLD2, TimerOutputs
 using Parameters, HDF5, Statistics, BSON, ValueHistories, ShapML
 using CUDA, Poppler_jll, MultivariateStats, Random, Suppressor
 using StatsBase, MLUtils, Flux, ExplainableAI, DataFrames
-using Zygote, Optimisers, ChainRulesCore, Interpolations, CairoMakie
+using Zygote, Optimisers, ChainRulesCore, Interpolations
 using TopOpt.TopOptProblems.InputOutput.INP.Parser: InpContent
 CUDA.allowscalar(false)
 println("\nDEFINITIONS...\n")
